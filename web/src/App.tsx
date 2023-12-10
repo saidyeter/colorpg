bee975a9ff8701ff4bccaa6efe5b88d4526464d1
said.yeter <said.yeter@obss.tech>
2023-12-10 22:35:22 +0300

    Implement single card

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,80 @@
-import './App.css'
 import { Button } from '@/components/ui/button'
+import { useState } from 'react';
+
+import { SketchPicker } from 'react-color';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from './components/ui/input';
+
+
 
 function App() {
+  const [text, setText] = useState('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec laoreet tellus a urna tempor, sit amet volutpat est volutpat. Praesent condimentum finibus massa, sit amet rhoncus lorem imperdiet id.')
+  const [textColor, setTextColor] = useState('#fff')
+  const [bgColor, setBgColor] = useState('#000')
+
   return (
-    <>
-      <div>color picker for text</div>
-      <div>color picker for bg</div>
-      <div>input for text</div>
-      <div>a div with selected bg color and text color that show text</div>
-      
-      <Button>Click me</Button>
-    </>
+    <div className='container mt-4  '>
+      <div className='flex'>
+
+        <div>
+          <Card>
+            <CardHeader>
+              <CardTitle>Text Color</CardTitle>
+              <CardDescription />
+            </CardHeader>
+            <CardContent>
+              <SketchPicker
+                color={textColor}
+                onChangeComplete={(d) => setTextColor(d.hex)}
+              />
+            </CardContent>
+            <CardFooter />
+          </Card>
+        </div>
+        <div>
+          <Card>
+            <CardHeader>
+              <CardTitle>Backgroud Color 1</CardTitle>
+              <CardDescription />
+            </CardHeader>
+            <CardContent>
+              <SketchPicker
+                color={bgColor}
+                onChangeComplete={(d) => setBgColor(d.hex)}
+              />
+            </CardContent>
+            <CardFooter />
+          </Card>
+        </div>
+      </div>
+      <div>
+        <Input value={text} onChange={(d) => setText(d.target.value)} />
+      </div>
+      <div>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Result</CardTitle>
+            <CardDescription />
+          </CardHeader>
+          <CardContent>
+
+            <div style={{ backgroundColor: bgColor }}>
+              <span style={{ color: textColor }}>{text}</span>
+            </div>
+          </CardContent>
+          <CardFooter />
+        </Card>
+      </div>
+
+    </div>
   )
 }
 
